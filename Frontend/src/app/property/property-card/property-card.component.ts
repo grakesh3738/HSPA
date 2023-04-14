0731e6409eb17203821eb05b282b1f49b3a3603e
Rakesh, Gutpe <grakesh3738@gmail.com>
2023-04-14 01:43:03 -0600

    Applied some css. Displayed multiple cards.

--- Frontend/src/app/property/property-card/property-card.component.ts
+++ Frontend/src/app/property/property-card/property-card.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
 @Component({
   selector: 'app-property-card',
   //template: '<h1>This is a property card component</h1>',
@@ -7,10 +7,5 @@ import {Component} from '@angular/core';
   styleUrls: ['property-card.component.css']
 })
 export class PropertyCardComponent{
-  Property: any = {
-    "Id": 1,
-    "Name": "Birla House",
-    "Type": "House",
-    "Price": 12000
-  }
+  @Input() property : any
 }
--- Frontend/src/app/property/property-list/property-list.component.ts
+++ Frontend/src/app/property/property-list/property-list.component.ts
@@ -6,5 +6,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./property-list.component.css']
 })
 export class PropertyListComponent {
-
+  properties: Array<any> = [
+    {
+      "Id": 1,
+      "Name": "Birla House",
+      "Type": "House",
+      "Price": 11000
+    },
+    {
+      "Id": 2,
+      "Name": "Erose Villa",
+      "Type": "Villa",
+      "Price": 12000
+    },
+    {
+      "Id": 3,
+      "Name": "Mark Hill",
+      "Type": "Flat",
+      "Price": 13000
+    },
+    {
+      "Id": 4,
+      "Name": "Birla House",
+      "Type": "House",
+      "Price": 14000
+    },
+    {
+      "Id": 5,
+      "Name": "Birla House",
+      "Type": "House",
+      "Price": 15000
+    },
+    {
+      "Id": 6,
+      "Name": "Birla House",
+      "Type": "House",
+      "Price": 16000
+    },
+    {
+      "Id": 7,
+      "Name": "Birla House",
+      "Type": "House",
+      "Price": 17000
+    },
+    {
+      "Id": 8,
+      "Name": "Birla House",
+      "Type": "House",
+      "Price": 18000
+    }
+  ]
 }
